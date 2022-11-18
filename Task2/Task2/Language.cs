07564ffa43547a20f1fa8869eb6d812c4866447f
jafarova1 <gultajvj@code.edu.az>
2022-11-18 13:20:32 -0800

    added file

---
+++ Task2/Task2/Language.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+   public class Language
+    {
+        public string Name { get; set; }
+        public DateTime CreatingDate { get; set; }
+        public int Power { get; set; }
+        public int Popularity { get; set; }
+        public Language(string name,DateTime creatingdate,int power,int popularity)
+        {
+            Name = name;
+            CreatingDate = creatingdate;
+            Power = power;
+            Popularity = popularity;
+        }
+        
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        public delegate T Power<T>(int power, string name);
+        public delegate T Date<T>(int power, string name);
+
+        static void Main(string[] args)
+        {
+           
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+        }
+        public static void GetUsefulLanguages(Dictionary<int, string> dictionary,Power<int> power,Date<DateTime> date)
+        {
+            
+
+        }
+        public static string CheckPower(int power,string name)
+        {
+            if (power > 100)
+            {
+                return $"{name} {power} yeterlidir ";
+            }
+            else
+            {
+                return $"{power} {name} yeterli deyil";
+            }
+        }
+        public static bool CheckCreatingDate(DateTime date)
+        {
+           DateTime date1=date.AddDays(-500);
+            
+            if (date1 == date)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+        public static int UpdatePopularity(int popularity)
+        {
+            return popularity * 2;
+        }
+    }
+}
